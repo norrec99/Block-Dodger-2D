7ab958a5fa3668b69a07d807328334b947857c5e
cagataysoyer <cagatay@stardust.gs>
2023-08-10 13:25:33 +0300

    Check if the object is outside the screen's bottom border

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera mainCamera;
+    private SpriteRenderer objectSpriteRenderer;
+
+    private void Start()
     {
-        
+        // Get the main camera and the object's renderer
+        mainCamera = Camera.main;
+        objectSpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 viewportPosition = mainCamera.WorldToViewportPoint(transform.position);
+
+        // Check if the object is outside the screen's bottom border
+        if (viewportPosition.y < 0f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
