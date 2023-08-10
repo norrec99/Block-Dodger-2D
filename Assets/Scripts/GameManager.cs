e33e8757ab9c938ffcf36731b9eb7e883e75e548
cagataysoyer <cagatay@stardust.gs>
2023-08-10 14:22:23 +0300

    spawn blocks

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,12 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    [SerializeField] private GameObject blockPrefab;
+    [SerializeField] private float maxX;
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private float spawnRate;
 
+    private bool isGameStarted = false;
 
     // Ensure the instance is not destroyed when loading a new scene
     private void Awake()
@@ -22,8 +27,31 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void Update() 
+    {
+        if (Input.GetMouseButton(0) && !isGameStarted)
+        {
+            StartSpawning();
+
+            isGameStarted = true;
+        }
+    }
+
     public void RestartGame()
     {
         SceneManager.LoadScene(0);
+    }
+
+    private void StartSpawning()
+    {
+        InvokeRepeating(nameof(SpawnBlock), 0.5f, spawnRate);
+    }
+
+    private void SpawnBlock()
+    {
+        Vector2 spawnPos = spawnPoint.position;
+        spawnPos.x = Random.Range(-maxX,maxX);
+
+        Instantiate(blockPrefab, spawnPos, Quaternion.identity);
     }
 }
